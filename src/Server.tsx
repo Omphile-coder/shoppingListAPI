07ef4ce0425115aa2cd30ebcebf8b543dd158af5
Omphile Lucas <lucasomphile23@gmail.com>
2026-09-09 13:14:59 +0200

    fixed syntax

--- src/Server.tsx
+++ src/Server.tsx
@@ -1,11 +1,11 @@
-import http, { IncomingMessage, ServerResponse } from 'http';
+import http, { IncomingMessage, ServerResponse } from "http";
 
 const PORT = 3000;
 
 const requestListener = (req: IncomingMessage, res: ServerResponse) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({ message: 'Hello, World!' }));
-}
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ message: "Hello, World!" }));
+};
 const server = http.createServer(requestListener);
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
